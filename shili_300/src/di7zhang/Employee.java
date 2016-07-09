c8616dec836ace11c0288daba3af84898a4cc3c5
whongj <wanghongjie@wanghongjiedeMacBook-Air.local>
2016-07-09 16:40:06 +0800

    闲得无聊

---
+++ shili_300/src/di7zhang/Employee.java
@@ -0,0 +1,26 @@
+package di7zhang;
+import java.util.*;
+public class Employee {
+	private String name;
+	private int salary;
+	private Date birthday;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	
+}
---
+++ shili_300/src/di7zhang/Manager.java
@@ -0,0 +1,14 @@
+package di7zhang;
+
+public class Manager extends Employee{
+	private double bonus;
+
+	public double getBonus() {
+		return bonus;
+	}
+
+	public void setBonus(double bonus) {
+		this.bonus = bonus;
+	}
+	
+}
